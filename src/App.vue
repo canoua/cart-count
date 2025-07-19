63869ce82751b296ef6ad53f107a8fc851ab80cf
akaryaka <akariaka@yandex.ru>
2025-07-19 22:50:35 +0600

    sidebar done

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <nav class="menu">
+  <button class="btn-menu" @click="menuIsActive">меню</button>
+  <nav class="menu" :class="{ 'open': menuActive }">
+    <button @click="menuIsActive" class="btn-close">x</button>
     <RouterLink class="menu__link" to="/">Выдача</RouterLink>
     <RouterLink class="menu__link" to="/refundview">Возврат</RouterLink>
     <RouterLink class="menu__link" to="/adddatabaseview">Добавить картридж</RouterLink>
@@ -10,24 +12,59 @@
   <RouterView />
 </template>
 
-<script setup>
+<script>
   import { RouterView } from 'vue-router';
+
+  export default {
+    data() {
+      return {
+        menuActive: false
+      }
+    },
+
+    methods: {
+      menuIsActive() {
+        this.menuActive = !this.menuActive
+      }
+    }
+  }
+  
 </script>
 
 <style scoped>
+  .btn-menu {
+    position: absolute;
+    top: 10px;
+    left: 10px;
+  }
+
+  .btn-close {
+    background-color: red;
+    display: inline-block;
+    width: 40px;
+    position: absolute;
+    right: 10px;
+    top: 10px;
+  }
+
   .menu {
-    transition: all .7s;
+    position: relative;
+    transition: all .5s;
     background-color: #fff;
     display: flex;
     flex-direction: column;
     justify-content: center;
     padding: 10px 0;
     position: absolute;
-    left: 0;
+    left: -100%;
     top: 0;
     height: 100vh;
     padding: 20px;
     box-shadow: 0px 2px 39px 13px rgba(205, 199, 255, 0.19);
+  }
+
+  .menu.open {
+    left: 0;
   }
 
   .menu__link {
