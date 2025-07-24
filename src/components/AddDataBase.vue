9de24716a30541240e42109e22c6bf1753b4fd5c
akaryaka <akariaka@yandex.ru>
2025-07-24 13:58:29 +0600

    tasks ref

--- src/components/AddDataBase.vue
+++ src/components/AddDataBase.vue
@@ -10,6 +10,10 @@
           </template>
         </select>
       </div>
+      <!-- <div class="input-wrapper"> -->
+        <!-- <p :class="{'input-placeholder_active': inputActive}" class="input-placeholder">Штрихкод</p> -->
+        <!-- <input @focus="placeholderToggle" v-on:blur="placeholderToggle" class="form-input" type="text"> -->
+      <!-- </div>   -->
       <input class="form-input" type="text" placeholder="штрихкод">
       <input class="form-btn" type="submit" value="добавить">
     </form>
@@ -25,6 +29,12 @@
           {id: 2, title: '725'},
           {id: 3, title: '703'},
         ],
+        inputActive: false
+      }
+    },
+    methods: {
+      placeholderToggle() {
+        this.inputActive = !this.inputActive;
       }
     }
   }
@@ -34,5 +44,25 @@
   .home-form__model {
     display: flex;
     flex-direction: column;
+  }
+
+    .input-wrapper {
+    position: relative;
+  }
+
+  .input-placeholder {
+    transition: all .3s;
+    position: absolute;
+    left: 10px;
+    top: 10px;
+    color: #a0a0a0;
+    pointer-events: none;
+  }
+
+  .input-placeholder_active {
+    font-size: 14px;
+    top: -11px;
+    background-color: #fff;
+    box-shadow: 2px 3px 2px 3px #fff;
   }
 </style>
