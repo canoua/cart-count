a35f06abdf7f6932efbe53f99c0c9aaa6211836a
unknown <akariaka@yandex.ru>
2025-08-12 23:44:32 +0600

    ref: add table in cartlist, cartlistall, refillinglist

--- src/components/CartList.vue
+++ src/components/CartList.vue
@@ -1,7 +1,104 @@
 <template>
   <div class="section">
-    <div class="form">
+    <form class="form" action="">
       <h2>Список картриджей, ожидающих заправку</h2>
-    </div>
+      <div class="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th scope="col">Модель</th>
+              <th scope="col">Штрихкод</th>
+              <th scope="col">Ячейка</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">TK-1200</th>
+              <td>21520000545662</td>
+              <td>2</td>
+              <td>
+                <input type="checkbox">
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">FX-10</th>
+              <td>21520000548562</td>
+              <td>5</td>
+              <td>
+                <input type="checkbox">
+              </td>
+            </tr>
+            <tr>
+              <th scope="row">12A</th>
+              <td>21520000548565</td>
+              <td>12</td>
+              <td>
+                <input type="checkbox">
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div> 
+      <input type="submit" value="отправить">
+    </form>
   </div>
 </template>
+
+<style scoped>
+  .table-container {
+    overflow: hidden;
+    border-radius: 10px;
+    border: 1px solid rgb(140 140 140);
+  }
+  table {
+    border-collapse: collapse;
+    width: 100%;
+    font-family: sans-serif;
+    font-size: 0.8rem;
+    letter-spacing: 1px;
+  }
+
+  caption {
+    caption-side: bottom;
+    padding: 10px;
+    font-weight: bold;
+  }
+
+  thead,
+  tfoot {
+    background-color: rgb(228 240 245);
+  }
+
+  th,
+  td {
+    border-right: 1px solid #000;
+    padding: 8px 10px;
+    border-radius: inherit;
+  }
+
+  th:last-child,
+  tr:last-child,
+  td:last-child {
+    border-right: none;
+  }
+
+  td:last-of-type {
+    text-align: center;
+  }
+
+  tbody > tr:nth-of-type(even) {
+    background-color: rgb(237 238 242);
+  }
+
+  tfoot th {
+    text-align: right;
+  }
+
+  tfoot td {
+    font-weight: bold;
+  }
+
+  .form__cart-list-all {
+    width: 60%;
+  }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,9 +2,9 @@
   <section class="section">
     <form class="form form-home" action="#">
       <h1>Выдача клиенту</h1>
-      <Input placeholder="Модель картриджа"/>
+      <!-- <Input placeholder="Модель картриджа"/> -->
       <Input placeholder="Штрихкод"/>
-      <Input placeholder="номер кабинета"/>
+      <!-- <Input placeholder="номер кабинета"/> -->
       <Input placeholder="инвентарный номер принтера/МФУ"/>
       <input class="form-btn" type="submit" value="выдать">
     </form>
